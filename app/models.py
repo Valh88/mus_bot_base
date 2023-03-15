3ea8076bdd702c387ab63b2d35ee5114b639d74b
vano <simplmd@gmail.com>
2023-03-16 00:07:58 +0700

    create_table

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ alembic upgrade head
 import uuid
 from typing import List
 import datetime
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey, Table, Column, Float
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy_utils import ChoiceType
@@ -38,6 +38,14 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String(128), nullable=False)
 
 
+association_table_band_genres = Table(
+    'association_table_band_genres',
+    Base.metadata,
+    Column('band_id', ForeignKey('bands.id'), primary_key=True),
+    Column('genre_id', ForeignKey('genres.id'), primary_key=True),
+)
+
+
 class Band(Base):
     __tablename__ = "bands"
 
@@ -54,10 +62,17 @@ class Band(Base):
     location: Mapped[str] = mapped_column(String(30), nullable=False)
     status: Mapped[ChoiceType] = mapped_column(ChoiceType(STATUS))
     formed_in: Mapped[datetime.datetime]
+    description: Mapped[str] = mapped_column(String(800))
     # # ears_active: Mapped[str]
-    # # genre
-    # # discography: Mapped[str]
+    genres: Mapped[List['Genre']] = relationship(
+        secondary=association_table_band_genres, back_populates='bands'
+    )
+    discography: Mapped[List['Album']] = relationship('Album', back_populates='band', cascade='delete, all')
     themes: Mapped[str] = mapped_column(String(100), nullable=False)
+    pictures: Mapped[List['BandPicture']] = relationship(
+        secondary='association_band_pictures', back_populates='bands'
+    )
+    picture_associations: Mapped[List['AssociationBandPicture']] = relationship(back_populates='band')
     commentary: Mapped[str] = mapped_column(String(300))
 
 
@@ -81,7 +96,13 @@ class Album(Base):
     label: Mapped[str] = mapped_column(String(30))
     format: Mapped[ChoiceType] = mapped_column(ChoiceType(FORMAT))
     limitation: Mapped[int] = mapped_column(Integer)
+    band_id: Mapped[str] = mapped_column(ForeignKey('bands.id'), nullable=False)
+    band: Mapped['Band'] = relationship('Band', back_populates='albums')
     songs: Mapped[List['Track']] = relationship('Track', back_populates='album', cascade='delete, all')
+    pictures: Mapped[List['AlbumPicture']] = relationship(
+        secondary='associations_album_picture', back_populates='albums'
+    )
+    picture_assotiations: Mapped[List['AssociationsAlbumPicture']] = relationship(back_populates='album')
     commentary: Mapped[str] = mapped_column(String(300))
 
 
@@ -93,5 +114,62 @@ class Track(Base):
     )
     name: Mapped[str] = mapped_column(String(60), nullable=False)
     num_song: Mapped[int] = mapped_column(Integer, nullable=False)
+    sound_time: Mapped[float] = mapped_column(Float(3))
     album_id: Mapped[str] = mapped_column(ForeignKey("albums.id"), nullable=False) 
     album: Mapped["Album"] = relationship("Album", back_populates="songs")
+
+
+class Genre(Base):
+    __tablename__ = 'genres'
+
+    id: Mapped[str] = mapped_column(
+        UUID(as_uuid=False), primary_key=True, default=lambda _: str(uuid.uuid4())
+    )
+    genre: Mapped[str] = mapped_column(String(20), nullable=False)
+    bands: Mapped[List['Band']] = relationship(
+        secondary=association_table_band_genres, back_populates='genres',
+    )
+
+
+class BandPicture(Base):
+    __tablename__ = 'band_pictures'
+
+    id: Mapped[str] = mapped_column(
+        UUID(as_uuid=False), primary_key=True, default=lambda _: str(uuid.uuid4())
+    )
+    path: Mapped[str] = mapped_column(String(60), nullable=True)
+    band: Mapped[List['Band']] = relationship(
+        secondary='association_band_pictures', back_populates='pictures'
+    )
+    band_associations: Mapped[List['AssociationBandPicture']] = relationship(back_populates='picture')
+
+
+class AlbumPicture(Base):
+    __tablename__ = 'album_pictures'
+
+    id: Mapped[str] = mapped_column(
+        UUID(as_uuid=False), primary_key=True, default=lambda _: str(uuid.uuid4())
+    )
+    path: Mapped[str] = mapped_column(String(60), nullable=True)
+    album: Mapped[List['Album']] = relationship(
+        secondary='associations_album_picture', back_populates='pictures'
+    )
+    album_associations: Mapped[List['AssociationsAlbumPicture']] = relationship(back_populates='picture')
+
+
+class AssociationsAlbumPicture(Base):
+    __tablename__ = 'associations_album_picture'
+
+    album_id: Mapped[str] = mapped_column(ForeignKey('albums.id'), primary_key=True)
+    picture_id: Mapped[str] = mapped_column(ForeignKey('album_pictures.id'), primary_key=True)
+    album: Mapped['Album'] = relationship(back_populates='picture_associations')
+    picture: Mapped['AlbumPicture'] = relationship(back_populates='album_assotiations')
+
+
+class AssociationBandPicture(Base):
+    __tablename__ = 'association_band_pictures'
+
+    band_id: Mapped[str] = mapped_column(ForeignKey('bands.id'), primary_key=True)
+    picture_id: Mapped[str] = mapped_column(ForeignKey('band_pictures.id'), primary_key=True)
+    band: Mapped['Band'] = relationship(back_populates='picture_associations')
+    picture: Mapped['BandPicture'] = relationship(back_populates='band_associations')
